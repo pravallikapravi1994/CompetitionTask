bb28b44c4f0c84704b24c93b15fb0ff075e6e4ed
Pravallika <pravallikapravi1994@gmail.com>
2020-06-03 11:51:20 +1200

    First commit for competition

---
+++ marsframework-master/MarsFramework/Pages/SignIn.cs
@@ -0,0 +1,47 @@
+﻿using MarsFramework.Global;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System.Threading;
+
+namespace MarsFramework.Pages
+{
+    class SignIn
+    {
+        public SignIn()
+        {
+            PageFactory.InitElements(Global.GlobalDefinitions.driver, this);
+        }
+
+        #region  Initialize Web Elements 
+        //Finding the Sign Link
+        [FindsBy(How = How.XPath, Using = "//A[@class='item'][text()='Sign In']")]
+        private IWebElement SignIntab { get; set; }
+
+        // Finding the Email Field
+        [FindsBy(How = How.XPath, Using = "(//INPUT[@type='text'])[2]")]
+        private IWebElement Email { get; set; }
+
+        //Finding the Password Field
+        [FindsBy(How = How.XPath, Using = "//INPUT[@type='password']")]
+        private IWebElement Password { get; set; }
+
+        //Finding the Login Button
+        [FindsBy(How = How.XPath, Using = "//BUTTON[@class='fluid ui teal button'][text()='Login']")]
+        private IWebElement LoginBtn { get; set; }
+
+        #endregion
+
+        internal void LoginSteps()
+        {
+            SignIntab.Click();
+            Thread.Sleep(3000);
+            Email.SendKeys("pravallika.p@test.com");
+            GlobalDefinitions.TurnOnWait();
+            Password.SendKeys("hello123");
+            GlobalDefinitions.TurnOnWait();
+            LoginBtn.Click(); 
+            GlobalDefinitions.TurnOnWait();
+
+        }
+    }
+}
---
+++ marsframework-master/MarsFramework/Test/Program.cs
@@ -0,0 +1,134 @@
+﻿using MarsFramework.Global;
+using MarsFramework.Pages;
+using NUnit.Framework;
+
+namespace MarsFramework
+{
+    public class Program
+    {
+        [TestFixture]
+        [Category("Sprint1")]
+        class User : Global.Base
+        {
+           
+            [Test, Order(1)]
+            public void AddShareSkillService()
+            {
+
+                GlobalDefinitions.TurnOnWait();
+
+                ShareSkill s = new ShareSkill();
+                s.EnterShareSkill();
+
+
+
+                GlobalDefinitions.TurnOnWait();
+
+                Profile profile1 = new Profile();
+                profile1.NavigateToManageListingpage();
+
+                
+                
+                GlobalDefinitions.TurnOnWait();
+
+
+
+                ManageListings ml = new ManageListings();
+                ml.VerifySavedServiceListings();
+
+
+            }
+
+
+
+            [Test, Order(2)]
+            public void EditShareSkill()
+            {
+
+                GlobalDefinitions.TurnOnWait();
+
+                Profile profile = new Profile();
+                profile.NavigateToManageListingpage();
+
+                GlobalDefinitions.TurnOnWait();
+
+
+              
+
+                
+                GlobalDefinitions.TurnOnWait();
+
+                ManageListings ml = new ManageListings();
+                ml.EditSharedSkill();
+
+
+                GlobalDefinitions.TurnOnWait();
+                profile.NavigateToManageListingpage();
+
+                GlobalDefinitions.TurnOnWait();
+                ml.VerifySavedServiceListings();
+
+               
+
+                GlobalDefinitions.TurnOnWait();     
+
+            }
+
+
+
+            [Test, Order(4)]
+            public void DeleteShareSkillIsDeleted()
+            {
+
+                GlobalDefinitions.TurnOnWait();
+
+                Profile profile = new Profile();
+                profile.NavigateToManageListingpage();
+
+                GlobalDefinitions.TurnOnWait();
+                GlobalDefinitions.wait(1000);
+
+                ManageListings ml = new ManageListings();
+                ml.DeleteSharedSkill();
+
+                GlobalDefinitions.TurnOnWait();
+                GlobalDefinitions.wait(1000);
+                ml.DeleteSharedYes();
+
+                GlobalDefinitions.TurnOnWait();
+                GlobalDefinitions.wait(1000);
+                ml.VerifyDeleted();
+
+
+            }
+
+            [Test, Order(3)]
+            public void DeleteShareSkillNotDeleted()
+            {
+
+                GlobalDefinitions.TurnOnWait();
+
+                Profile profile = new Profile();
+                profile.NavigateToManageListingpage();
+
+                GlobalDefinitions.TurnOnWait();
+
+                ManageListings ml = new ManageListings();
+                GlobalDefinitions.wait(1000);
+                ml.DeleteSharedSkill();
+
+                GlobalDefinitions.TurnOnWait();
+                GlobalDefinitions.wait(1000);
+                ml.DeleteSharedSkillNo();
+
+                GlobalDefinitions.TurnOnWait();
+                GlobalDefinitions.wait(1000);
+                ml.VerifyNotDeleted();
+
+
+            }
+
+
+        }
+    }
+}
